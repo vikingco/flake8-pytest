0de9e99fa74f1c28f9ea57f96fa975785260a170
Niels Van Och <niels.vanoch@gmail.com>
2016-06-08 10:57:54 +0200

    Initial commit
    Add the first version of the plugin. Should be feature-complete.

---
+++ flake8_pytest.py
@@ -0,0 +1,56 @@
+import ast
+import tokenize
+
+from sys import stdin
+
+__version__ = '1.0'
+
+PYTEST_ERROR_CODE = 'T003'
+PYTEST_ERROR_MESSAGE = 'use of Django-style assert statement found (instead of regular assert)'
+ASSERTS = ('assertAlmostEqual', 'assertAlmostEquals', 'assertContains', 'assertDictContainsSubset', 'assertDictEqual',
+           'assertEqual', 'assertEquals', 'assertFalse', 'assertFieldOutput', 'assertFormError', 'assertFormsetError',
+           'assertGreater', 'assertGreaterEqual', 'assertHTMLEqual', 'assertHTMLNotEqual', 'assertIn', 'assertInHTML',
+           'assertIs', 'assertIsInstance', 'assertIsNone', 'assertIsNot', 'assertIsNotNone', 'assertItemsEqual',
+           'assertJSONEqual', 'assertJSONNotEqual', 'assertLess', 'assertLessEqual', 'assertListEqual',
+           'assertMultiLineEqual', 'assertNotAlmostEqual', 'assertNotAlmostEquals', 'assertNotContains',
+           'assertNotEqual', 'assertNotEquals', 'assertNotIn', 'assertNotIsInstance', 'assertNotRegexpMatches',
+           'assertNumQueries', 'assertQuerysetEqual', 'assertRaises', 'assertRaisesMessage', 'assertRaisesRegexp',
+           'assertRedirects', 'assertRegexpMatches', 'assertSequenceEqual', 'assertSetEqual', 'assertTemplateNotUsed',
+           'assertTemplateUsed', 'assertTrue', 'assertTupleEqual', 'assertXMLEqual', 'assertXMLNotEqual', 'assert_')
+
+
+class PytestAssertChecker(object):
+    name = 'flake8-pytest'
+    version = __version__
+
+    def __init__(self, tree, filename='(none)', builtins=None):
+        self.tree = tree
+        self.filename = (filename == 'stdin' and stdin) or filename
+
+    def run(self):
+        # Get lines to ignore
+        if self.filename == stdin:
+            noqa = _get_noqa_lines(self.filename)
+        else:
+            with open(self.filename, 'r') as file_to_check:
+                noqa = _get_noqa_lines(file_to_check.readlines())
+
+        # Run the actual check
+        errors = []
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute) and node.func.attr in ASSERTS and node.lineno not in noqa:
+                errors.append({
+                    "message": '{0} {1}: {2}'.format(PYTEST_ERROR_CODE, PYTEST_ERROR_MESSAGE, node.func.attr),
+                    "line": node.lineno,
+                    "col": node.col_offset
+                })
+
+        # Yield the found errors
+        for error in errors:
+            yield (error.get("line"), error.get("col"), error.get("message"), type(self))
+
+
+def _get_noqa_lines(code):
+    tokens = tokenize.generate_tokens(lambda L=iter(code): next(L))
+    return [token[2][0] for token in tokens if token[0] == tokenize.COMMENT and
+            (token[1].endswith('noqa') or (isinstance(token[0], str) and token[0].endswith('noqa')))]
---
+++ setup.py
@@ -0,0 +1,51 @@
+from __future__ import with_statement
+from setuptools import setup
+
+
+def get_version(fname='flake8_pytest.py'):
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+
+
+def get_long_description():
+    descr = []
+    for fname in ('README.rst',):
+        with open(fname) as f:
+            descr.append(f.read())
+    return '\n\n'.join(descr)
+
+install_requires = ['flake8']
+
+setup(
+    name='flake8-pytest',
+    version=get_version(),
+    description="pytest assert checker plugin for flake8",
+    long_description=get_long_description(),
+    keywords='flake8 pytest',
+    author='VikingCo NV',
+    author_email='technology@vikingco.com',
+    url='https://github.com/vikingco/flake8-pytest',
+    license='MIT',
+    py_modules=['flake8_pytest'],
+    zip_safe=False,
+    entry_points={
+        'flake8.extension': [
+            'flake8_pytest = flake8_pytest:PytestAssertChecker',
+        ],
+    },
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
